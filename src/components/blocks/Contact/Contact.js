18dc556304a8d7f2ab391a8f08a5813f454539db
Nick East <nickeast1998@gmail.com>
2023-02-05 17:24:18 +0000

    latest

--- src/components/blocks/Contact/Contact.js
+++ src/components/blocks/Contact/Contact.js
@@ -1,10 +1,15 @@
-import { MaxWidth } from 'components/global'
 import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { useForm } from 'react-hook-form'
+
+import { MaxWidth } from 'components/global'
 
 const ContactStyles = styled.section`
   width: 100%;
   min-height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
   .contact {
     h3 {
       color: var(--white);
@@ -12,6 +17,56 @@ const ContactStyles = styled.section`
       margin-bottom: 1rem;
       font-size: 2rem;
       text-decoration: underline var(--main);
+    }
+    form {
+      margin-top: 3rem;
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+      padding-bottom: 3rem;
+      div {
+        p {
+          color: var(--white);
+          font-size: 1.5rem;
+          font-weight: 600;
+          margin-bottom: 1rem;
+        }
+        input,
+        textarea {
+          background: none;
+          width: 100%;
+          border: none;
+          border-bottom: solid 2px rgba(255, 255, 255, 0.5);
+          color: var(--white);
+          font-size: 1.5rem;
+          padding-bottom: 1rem;
+          &::placeholder {
+            color: rgba(255, 255, 255, 0.5);
+          }
+        }
+        textarea {
+          resize: vertical;
+          max-height: 300px;
+          min-height: 150px;
+        }
+      }
+      .button {
+        max-width: 500px;
+        margin: 0 auto;
+        width: 100%;
+        button {
+          width: 100%;
+          border-radius: 100px;
+          background-color: var(--main);
+          padding: 1.25rem 0;
+          border: none;
+
+          span {
+            font-weight: 700;
+            font-size: 1.5rem;
+          }
+        }
+      }
     }
   }
 `
@@ -33,9 +88,32 @@ const Contact = () => {
   })
   return (
     <ContactStyles>
-      <MaxWidth>
+      <MaxWidth maxWidth="900">
         <div className="contact">
-          <h3>Get in touch title</h3>
+          <h3>Let’s talk about your project!</h3>
+          <form action="">
+            <div>
+              <p>Your Name</p>
+              <input type="text" placeholder="Your Name" />
+            </div>
+            <div>
+              <p>Your Email</p>
+              <input type="text" placeholder="Your Email" />
+            </div>
+            <div>
+              <p>Your Company</p>
+              <input type="text" placeholder="Your Company" />
+            </div>
+            <div>
+              <p>About your project</p>
+              <textarea placeholder="Your Message" />
+            </div>
+            <div className="button">
+              <button type="submit">
+                <span>SUBMIT</span>
+              </button>
+            </div>
+          </form>
         </div>
       </MaxWidth>
     </ContactStyles>
