974e60cda8ece3efefebcd1ae76919cb94f2ff83
Nick East <nickeast1998@gmail.com>
2023-03-08 12:26:11 +0000

    latest

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,11 +7,6 @@ const IndexPage = () => {
   const [open, setOpen] = useState(false)
   return (
     <Layout>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
-        voluptates error fuga laborum non ipsam rem maxime eveniet, tempore unde
-        sunt recusandae deleniti voluptas aliquid.
-      </p>
       <Hero />
       <About />
       <Projects />
